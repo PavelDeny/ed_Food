209f709606f2f64ada05bc230ae22ed92929f180
Pavel D. <paveldenysenko91@gmail.com>
2025-02-17 12:15:37 +0200

    создание табов

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener("DOMContentLoaded", () => {
+  //------------------------------------------------
+  //  Tabs
+
+  const tabsContent = document.querySelectorAll(".tabcontent");
+  const tabsParent = document.querySelector(".tabheader__items");
+  const tabs = tabsParent.querySelectorAll(".tabheader__item");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.remove("show", "fade");
+      item.classList.add("hide");
+    });
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (event) => {
+    let target = event.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+  //------------------------------------------------
+});
